0e247a24cc9b0f2cd30325932fe401c81ed518d1
alexistirapelle <alexistirapelle@gmail.com>
2022-01-30 14:55:55 -0300

    ADD form

--- passagens/views.py
+++ passagens/views.py
@@ -1,6 +1,8 @@
+from django import forms
 from django.shortcuts import render
 from passagens.forms import PassagemForms
 
 def index(request):
     form = PassagemForms()
-    return render(request, 'index.html')
+    contexto = {'form':form}
+    return render(request, 'index.html', contexto)
